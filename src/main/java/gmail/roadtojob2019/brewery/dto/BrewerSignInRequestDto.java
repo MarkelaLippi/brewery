01d9c6f1de4902bff8ec384dc6d9b8a57405478b
MarkelaLippi <MarkelaLippi@gmail.com>
2020-02-19 22:07:29 +0300

    Delete extra code

--- src/main/java/gmail/roadtojob2019/brewery/dto/BrewerSignInRequestDto.java
+++
@@ -1,9 +0,0 @@
-package gmail.roadtojob2019.brewery.dto;
-
-import lombok.Data;
-
-@Data
-public class BrewerSignInRequestDto {
-    private String email;
-    private String password;
-}
--- src/main/java/gmail/roadtojob2019/brewery/dto/SalesSignInRequestDto.java
+++
@@ -1,9 +0,0 @@
-package gmail.roadtojob2019.brewery.dto;
-
-import lombok.Data;
-
-@Data
-public class SalesSignInRequestDto {
-    private String email;
-    private String password;
-}
