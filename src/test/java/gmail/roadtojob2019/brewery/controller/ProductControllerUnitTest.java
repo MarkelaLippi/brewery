c5436fd73e73d9ead0b6e4f615a7dd935ad6f3d6
Markelov Sergey <S_markelov@tut.by>
2020-02-26 12:10:07 +0300

    Add unit test (testGetPriselistIsOk)

---
+++ src/test/java/gmail/roadtojob2019/brewery/controller/ProductControllerUnitTest.java
@@ -0,0 +1,103 @@
+package gmail.roadtojob2019.brewery.controller;
+
+import gmail.roadtojob2019.brewery.entity.Product;
+import gmail.roadtojob2019.brewery.entity.Type;
+import gmail.roadtojob2019.brewery.entity.Unit;
+import gmail.roadtojob2019.brewery.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.willReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class ProductControllerUnitTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    @Test
+    void testGetPriselistIsOk() throws Exception {
+        // given
+        //signInAsCustomer();
+        final Product product = Product.builder()
+                .id(1L)
+                .name("CoolBeer")
+                .description("Light, 4.8% alcohol...")
+                .price(2.5)
+                .type(Type.BEER)
+                .unit(Unit.LITRE)
+                .build();
+
+        List<Product> products = List.of(product);
+
+        willReturn(products).given(productRepository)
+                .findByType(Type.BEER);
+        // when
+        mockMvc.perform(get("/brewery/customer/pricelist"))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(content().json("[\n" +
+                        "  {\n" +
+                        "    \"beerId\" : 1, \n" +
+                        "    \"beerName\" : \"CoolBeer\",\n" +
+                        "    \"beerDescription\" : \"Light, 4.8% alcohol...\",\n" +
+                        "    \"price\" : 2.5\n" +
+                        "  }\n" +
+                        "]"));
+    }
+
+    @Test
+    void testGetAllProductsByTypeIsOk() throws Exception {
+        mockMvc.perform(get("/brewery/sales/products?type=beer"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[\n" +
+                        "  {\n" +
+                        "    \"id\" : 1, \n" +
+                        "    \"name\" : \"CoolBeer\",\n" +
+                        "    \"description\" : \"Light, 4.8% alcohol...\",\n" +
+                        "    \"price\" : 2.5,\n" +
+                        "    \"amount\" : 500.0,\n" +
+                        "    \"unit\" : \"LITRE\" \n" +
+                        "  }\n" +
+                        "]"));
+    }
+
+    @Test
+    void testGetProductByIdIsOk() throws Exception {
+        mockMvc.perform(get("/brewery/brewer/products/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        "  {\n" +
+                                "    \"id\" : 2, \n" +
+                                "    \"name\" : \"Water\",\n" +
+                                "    \"description\" : \"Artesian, ...\",\n" +
+                                "    \"amount\" : 800.0,\n" +
+                                "    \"unit\" : \"LITRE\" \n" +
+                                "  }\n"));
+    }
+
+    @Test
+    public void testChangeProductAmountIsOk() throws Exception {
+        mockMvc.perform(patch("/brewery/brewer/products/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(" {\n" +
+                        "    \"amount\" : 250\n" +
+                        "  }\n"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("1"));
+    }
+}
