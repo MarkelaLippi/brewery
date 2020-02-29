79400d5510b6f57fde6e730aa994953b49833a79
MarkelaLippi <MarkelaLippi@gmail.com>
2020-03-01 00:49:14 +0300

    Delete extra code

--- src/main/java/gmail/roadtojob2019/brewery/service/AuthInfoService.java
+++
@@ -1,9 +0,0 @@
-package gmail.roadtojob2019.brewery.service;
-
-import gmail.roadtojob2019.brewery.entity.AuthInfoEntity;
-
-import java.util.Optional;
-
-public interface AuthInfoService {
-    Optional<AuthInfoEntity> findByLogin(String username);
-}
