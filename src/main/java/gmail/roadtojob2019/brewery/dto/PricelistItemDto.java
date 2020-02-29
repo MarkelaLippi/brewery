2dd86ec0c92842b41aae9016c9a97e44212d56b3
MarkelaLippi <MarkelaLippi@gmail.com>
2020-02-29 23:43:11 +0300

    Delete extra code

--- src/main/java/gmail/roadtojob2019/brewery/dto/PricelistItemDto.java
+++ src/main/java/gmail/roadtojob2019/brewery/dto/PricelistItemDto.java
@@ -1,10 +1,8 @@
 package gmail.roadtojob2019.brewery.dto;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class PricelistItemDto {
     private Long beerId;
     private String beerName;
